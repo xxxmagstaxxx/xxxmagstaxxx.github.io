32a676f3d8a68d2aa6ae5b8b52f197af4266141a
magsta <70142317+xxxmagstaxxx@users.noreply.github.com>
2024-08-24 15:38:20 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const includeHeader = new XMLHttpRequest();
+includeHeader.open("GET", "header.html", true);
+includeHeader.onreadystatechange = function () {
+  if (includeHeader.readyState === 4 && includeHeader.status === 200) {
+    const headerHTML = includeHeader.responseText;
+    const header = document.querySelector("#header");
+    header.insertAdjacentHTML("afterbegin", headerHTML);
+  }
+};
+includeHeader.send();
